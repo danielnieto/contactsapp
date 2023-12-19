de8437ff320a89098b0dda400ca58501eadcdfb9
Daniel Nieto <danielnieto89@gmail.com>
2023-12-18 22:11:01 -0600

    add basic layout and search functionality

--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
+from contacts.models import Contact
+from django.db.models import  Q
 
 def index(request):
     return redirect("contacts")
 
 def contacts(request):
-    return HttpResponse("Contacts")
+    search = request.GET.get('q')
+
+    if search:
+        contacts = Contact.objects.filter(
+                Q(first__icontains=search) |
+                Q(last__icontains=search) |
+                Q(phone__icontains=search) |
+                Q(email__icontains=search)
+            )
+    else:
+        contacts = Contact.objects.all()
+    return render(request, "contacts/index.html", {'contacts': contacts, 'search': search})
