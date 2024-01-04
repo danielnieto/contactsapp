a6a94d6a427c9112e203cd2cf170d64ff0736649
Daniel Nieto <danielnieto89@gmail.com>
2024-01-03 18:03:31 -0600

    create new contact

--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from contacts.models import Contact
 from django.db.models import Q
 from django.views import View
+from django.contrib import messages
 
 
 def index(request):
@@ -28,3 +30,13 @@ def contacts(request):
 class ContactsNewView(View):
     def get(self, request, *args, **kwargs):
         return render(request, "contacts/new.html", {"contact": Contact()})
+
+    def post(self, request, *args, **kwargs):
+        Contact.objects.create(
+            first=request.POST.get("first_name"),
+            last=request.POST.get("last_name"),
+            phone=request.POST.get("phone"),
+            email=request.POST.get("email"),
+        )
+        messages.success(request, "Created New Contact!")
+        return redirect(reverse("contacts_new"))
