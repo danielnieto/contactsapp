ae7988c5131708977481de049c1e1a1fe7f96c81
Daniel Nieto <danielnieto89@gmail.com>
2024-01-15 23:11:47 -0600

    make email unique

--- contacts/models.py
+++ contacts/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     first = models.CharField(max_length=50)
     last = models.CharField(max_length=50)
     phone = models.CharField(max_length=15)  # Assuming phone numbers as strings
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return f"{self.first} {self.last}"
